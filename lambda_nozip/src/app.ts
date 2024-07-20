654e79608b065f0866f37775298447104d8eff72
nbari <nbari@tequila.io>
2024-07-20 07:54:18 +0200

    Update app.ts
    hello 1 -- testing pipeline

--- lambda_nozip/src/app.ts
+++ lambda_nozip/src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World, hello Fraternitas!');
+  res.send('Hello 1');
 });
 
 export default app;
