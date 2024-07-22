f2f12c48922019c9df0bf1a92ba76243322736d8
nbari <nbari@tequila.io>
2024-07-22 19:29:01 +0200

    Update app.ts
    test 2

--- lambda_nozip/src/app.ts
+++ lambda_nozip/src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello 1');
+  res.send('Hello 2');
 });
 
 export default app;
